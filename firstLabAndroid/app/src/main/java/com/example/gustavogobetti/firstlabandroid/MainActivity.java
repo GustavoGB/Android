c6abe45740078811a94512647ab6fdbb824ed089
GustavoGB <gustavo.gobetti98@gmail.com>
2018-12-07 12:07:18 -0800

    Bug with the buttons- just change the color once

--- firstLabAndroid/app/src/main/java/com/example/gustavogobetti/firstlabandroid/MainActivity.java
+++ firstLabAndroid/app/src/main/java/com/example/gustavogobetti/firstlabandroid/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -21,40 +22,68 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void toast(String message){
-        Toast.makeText(this, "Left!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 
+//
+//    //RButton
+//    Button Rbutton = findViewById(R.id.right_button);
+//    //Lbutton
+//    Button Lbutton = findViewById(R.id.left_button);
+    //RandomIndex
+    //public static final Random RANDOM = new Random();
+    //Set the view as a ROOT
 
-    //Declaring the buttons
-    //Rbutton = (Button) findViewById(R.id.right_button);
-    //
-    //Button Lbutton = findViewById(R.id.left_button);
-    //Method for buttonClicked--
-    //1step
 
     //Create a Tuple with the value of the Hexadecimal of the color and the name of it.
-
-
-
-    String[] arrayName   = {"Green","Blue","Red","Black","White","Gray", "LightGray", "DarkGray", "Magenta"};
-    int [] arrayColors   = {Color.GREEN,Color.BLUE,Color.RED,Color.BLACK,Color.WHITE,Color.WHITE,Color.GRAY,Color.LTGRAY,Color.DKGRAY,Color.MAGENTA};
+    // 2 Strings instead of the Tuple
+    //String[] arrayName   = {"Green","Blue","Red","Black","White","Orange", "Pink", "Purple", "Brown"};
     //String[] arrayColors = {"#00FF00","#0000FF","#FF0000","000000","#FF0000", "#FF4E00", "#FF00FF", "#800080", "#6A280B"};
 
 
-    public static int getRandom( int [] arrayColors) {
-        int randomColor = new Random().nextInt(arrayColors.length);
-        return arrayColors[randomColor];
-    }
+    ////int [] arrayColors   = {Color.GREEN,Color.BLUE,Color.RED,Color.BLACK,Color.WHITE,Color.WHITE,Color.GRAY,Color.LTGRAY,Color.DKGRAY,Color.MAGENTA};
 
+    //
+    public int counter = 0;
+
+    //RandomIndex
+    int randomRight = (int) (Math.random()*10);
+    int randomLeft  = (int) (Math.random()*10);
+
+    //Arrays of name and the actual Colors
+    String[] arrayName   = {"Green","Blue","Red","Black","White","Orange", "Pink", "Purple", "Brown"};
+    String[] arrayColors = {"#00FF00","#0000FF","#FF0000","000000","#FF0000", "#FF4E00", "#FF00FF", "#800080", "#6A280B"};
 
     public void buttonClicked(View view) {
-        if (view.getId() == R.id.left_button){
-            //Button rightBtn = (arrayColors);
-            toast("Left!");
+        //Buttons and layout
+        Button Rbutton = findViewById(R.id.right_button);
+        //Lbutton
+        Button Lbutton = findViewById(R.id.left_button);
+        //Color Name Display
+        TextView mColorName = (TextView) findViewById(R.id.colorName);
+        //Score Display
+        TextView mScore     = (TextView) findViewById(R.id.score) ;
+
+
+        //Rbutton.setBackgroundColor(Color.parseColor(arrayName[randomRight]));
+        //Lbutton.setBackgroundColor(Color.parseColor(arrayColors[randomLeft]));
+
+        if (arrayColors[randomRight].equals(arrayName)) {
+            mColorName.setText(arrayColors[randomRight]);
+            mScore.setText(Integer.toString(counter));
+            Rbutton.setBackgroundColor(Color.parseColor(arrayColors[randomRight]));
+            Lbutton.setBackgroundColor(Color.parseColor(arrayColors[randomLeft]));
+            toast("Right!");
+            counter++;
         }
         else{
-            toast("Right!");
+            mColorName.setText(arrayColors[randomLeft]);
+            Rbutton.setBackgroundColor(Color.parseColor(arrayColors[randomRight]));
+            Lbutton.setBackgroundColor(Color.parseColor(arrayColors[randomLeft]));
+            toast("Wrong");
         }
     }
+
+
 
 }
