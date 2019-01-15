2e41229be148cf019fe094a655731ab0121dcaa8
GustavoGB <gustavo.gobetti98@gmail.com>
2019-01-14 16:38:13 -0800

    Making more activities

--- ReGroup/app/src/main/java/com/example/gustavogobetti/re_group/PresentationActivity.kt
+++ ReGroup/app/src/main/java/com/example/gustavogobetti/re_group/PresentationActivity.kt
@@ -1,12 +1,19 @@
 package com.example.gustavogobetti.re_group
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class presentationScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.present_page)
+    }
+
+    fun signIn(view: View) {
+        val intent = Intent(this, loginScreen::class.java)
+        startActivity(intent)
     }
 }
